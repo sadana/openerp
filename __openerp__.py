7abe476296c0e010a462b0c43faecb954754a661
sadana <mdthanh@yahoo.com>
2014-03-27 16:57:48 +0700

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,11 @@
+{
+    'name': "Product extended",
+    'version': "1.0",
+	  'description': "This module will extend the product's properties to adapt business demand",
+    'author': "ERPTalk team",
+	  'website': "erptalk.net",
+    'category': "Tools",
+    'depends': ['product'],
+    'data': ['product_extended.xml'],
+    'installable': True,
+}
